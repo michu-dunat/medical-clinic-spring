9499b120f6ce978f9dc1d6db97b49e8c6e82b740
Paullo99 <macionczyk.paw@gmail.com>
2020-12-30 17:04:19 +0100

    added a happy doctor

--- src/main/java/com/company/GreetingController.java
+++ src/main/java/com/company/GreetingController.java
@@ -2,19 +2,16 @@ package com.company;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 
-@RestController
+@Controller
 public class GreetingController {
 
     @RequestMapping("/greeting")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
-
 }
